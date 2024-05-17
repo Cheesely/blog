e626b3b853ec7829e0da9f797e948cedd925aa8d
chelseypnw <chelynnfoster@gmail.com>
2024-05-16 21:28:46 -0700

    Give user ownership over comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
+    @comment = @article.comments.create(comment_params.merge(user_id: current_user.id))
     redirect_to article_path(@article)
   end
 
@@ -9,8 +9,8 @@ class CommentsController < ApplicationController
     if current_user.blank?
       render plain: "You must be logged in to delete a comment", status: :unauthorized
     else
-      @article = Article.find(params[:article_id])
-      @comment = @article.comments.find(params[:id])
+      @article = Article.find(comment_params[:article_id])
+      @comment = @article.comments.find(comment_params[:id])
       @comment.destroy
       redirect_to article_path(@article), status: :see_other
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password :password, validations:true
   has_many :articles
+  has_many :comments
   validates :name, presence: true, uniqueness: true
 end
